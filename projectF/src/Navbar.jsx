bfa954aa95a588b6e43f65e6b704e65fe00f148a
Phatsaraporn <phatsaraporn.j@gmail.com>
2023-04-17 15:56:11 +0700

    add layout navbar jsx

--- projectF/src/Navbar.jsx
+++ projectF/src/Navbar.jsx
@@ -1,14 +1,24 @@
-
+import {logo} from "./picture/logo.svg"
 
 const Navbar = () => {
     return (
-        <div id="navbar">
-            <ul id="navbar-right">
-                <li><a href={''}>Home</a></li>
-                <li><a href={''}>About us</a></li>
-                <li><a href={''}>Contact</a></li>
-                <li><a href={''}>Login</a></li>
+        <nav class="navbar">
+            <div class="left-navbar-dt">
+                <a href={'/'}>
+                    <div>
+                        <img src={logo} alt="YUNWHANG-logo"  />
+                    </div>
+                    <p><b>YUNWHANG</b></p>
+                </a>
+            </div>
+            <ul class="right-navbar-dt">
+                <li><a href={'/'}><b>Home</b></a></li>
+                <li><a href={''}><b>About Us</b></a></li>
+                <li><a href={''}><b>Contact</b></a></li>
+                <li><a href={'/Login'}><button type="button" id="navbar-login-btn"><b>Log In</b></button></a></li>
             </ul>
-        </div>
+        </nav>
     )
 }
+
+export default Navbar
--- projectF/src/main.jsx
+++ projectF/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Home from './Home';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+  // {
+  //   path: '/Login',
+  //   element: <Login />
+  // },
+  // {
+  //   path: '/Register',
+  //   element: <Register />
+  // }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <UserContext>
+    <RouterProvider router={router} />
+  </UserContext>
 )
