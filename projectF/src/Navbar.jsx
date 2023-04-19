b38d7c2f424597a88b111c8eef6450276f3e173c
Bright <108338051+I3right@users.noreply.github.com>
2023-04-19 10:36:20 +0700

    Add Navbar css

--- projectF/src/Navbar.jsx
+++ projectF/src/Navbar.jsx
@@ -1,19 +1,20 @@
-import mainLogo from './picture/logo.svg'
+import mainLogo from "./assets/picture/logo.svg";
+import './assets/CSS/Navbar.css'
 
 const Navbar = () => {
-    return (
-        <nav class="navbar">
-            <div>
-                <img src={mainLogo} alt='main Logo'/>
-            </div>
-            <ul class="right-navbar-dt">
-                <li><a href={'/'}><b>Home</b></a></li>
-                <li><a href={'/About'}><b>About Us</b></a></li>
-                <li><a href={''}><b>Contact</b></a></li>
-                <li><a href={''}><button type="button" id="navbar-login-btn"><b>Log In</b></button></a></li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar">
+      <div>
+        <img src={mainLogo} alt="main Logo" />
+      </div>
+      <ul className="right-navbar-dt">
+        <li><a href={"/"}><b>Home</b></a></li>
+        <li><a href={"/About"}><b>About Us</b></a></li>
+        <li><a href={""}><b>Contact</b></a></li>
+        <li><a href={""}><button type="button" id="navbar-login-btn"><b>Log In</b></button></a></li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
